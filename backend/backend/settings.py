113502cacf1bffcfd40a98a483f824055370cf8c
unknown <24b0059@iitb.ac.in>
2026-03-22 16:53:18 +0530

    Hosting changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,6 +60,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_createsuperuser',
+    'whitenoise.runserver_nostatic',
     'corsheaders',
     'rest_framework',
     'apis',
@@ -147,6 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
